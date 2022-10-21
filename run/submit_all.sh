44eaba2d0dc97db1c2e22eb60810653aa138e727
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-10-21 07:57:44 -0400

    update main results using optimally tuned parameters (#6)
    * add model specific optimal parameters
    
    * refactor parse_params to process and return both paths and params
    
    * fix main run script
    
    - Fix gml models options
    - Allocate extra time for GCN when using STRING network
    - Use NOAUTO constraint option
    
    * upload results using optimally tuned hyperparameters
    
    * update main results notebook

--- run/submit_all.sh
+++ run/submit_all.sh
@@ -4,7 +4,7 @@ mkdir -p ../slurm_history
 
 networks=(BioGRID HumanNet STRING)
 labels=(DisGeNet GOBP)
-gml_models=(ADJ-LogReg ADJ-SVM N2V-LogReg N2V-SVM, LabelProp)
+gml_models=(ADJ-LogReg ADJ-SVM N2V-LogReg N2V-SVM LabelProp)
 gnn_models=(GCN GIN GAT GraphSAGE)
 
 function submit_job {
@@ -18,11 +18,12 @@ function submit_job {
 
     script="/bin/time -v python main.py network=${network} label=${label} model=${model}"
 
-    if (( gpu == 1 ))
-    then
+    if (( gpu == 1 )); then
         sbatch -J $job_name --gres=gpu:v100:1 job_template.sb $script
+    elif (( gpu == 2 )); then
+        sbatch -J $job_name --gres=gpu:v100:1 -t 12:00:00 job_template.sb $script
     else
-        sbatch -J $job_name -C amd20 job_template.sb $script
+        sbatch -J $job_name -C NOAUTO:amd20 job_template.sb $script
     fi
 }
 
@@ -30,10 +31,12 @@ for network in ${networks[@]}; do
     for label in ${labels[@]}; do
         for model in ${gnn_models[@]}; do
             if [[ $network == STRING ]] && [[ $model == GAT ]]; then
-                continue  # OOM
+                continue  # OOM on V100
+            elif [[ $network == STRING ]] && [[ $model == GCN ]]; then
+                submit_job $network $label $model 2
+            else
+                submit_job $network $label $model 1
             fi
-
-            submit_job $network $label $model 1
         done
 
         for model in ${gml_models[@]}; do
