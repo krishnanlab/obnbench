91b4788ca9103dc88c4eb810466af9e5e6ac5c42
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2023-02-16 16:53:33 -0500

    fix `n2v` and rerun `adj-logreg` for `DisGeNET_BEFREE` (#9)
    * fix num_woerker getter function
    
    Previously did not change 0 workers to default number of workers, causing issue with w2v
    
    * update results: fixed n2v runs
    
    * update logs
    
    * update notebook

--- utils.py
+++ utils.py
@@ -29,12 +29,14 @@ def get_device(device: str) -> str:
 
 def get_num_workers(num_workers: int) -> int:
     default_num_workers = numba.config.NUMBA_DEFAULT_NUM_THREADS
-    if num_workers <= default_num_workers:
+    if 0 < num_workers <= default_num_workers:
         return num_workers
-    elif num_workers != 0:
+    elif num_workers > default_num_workers:
         logger.warning(
             f"The specified {num_workers=} exceeds the maximum number of "
             f"available threads {default_num_workers}. "
             f"Setting to {default_num_workers} instead.",
         )
+    elif num_workers < 0:
+        raise ValueError(f"num_workers should be a non-negative, got {num_workers}")
     return default_num_workers
